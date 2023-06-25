8a9c2e0f54b282f649c1f258d5d1c16ea6d82f48
rsmith1631 <rsmith1631@gmail.com>
2023-06-25 13:38:14 -0700

    Added favNumber project practice1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace SelectionStatementExercise
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            favNumber = 25;
+            Console.WriteLine("Guess My Favorite Number (Guess any number from 1 to 1000)");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"Too Low");
+            }
+
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"Too High");
+            }
+
+            else
+            {
+                Console.WriteLine($"You guessed it!");
+            }
+
         }
     }
 }
+
