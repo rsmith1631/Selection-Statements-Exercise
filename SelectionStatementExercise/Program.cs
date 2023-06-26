cebeda2b87063717717a56d5c2605f50a67c15e6
rsmith1631 <rsmith1631@gmail.com>
2023-06-26 11:23:19 -0700

    Made some corrections

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What's my favorite number?");
+
+            var myNumber = 34;
+            var userInput = int.Parse(Console.ReadLine());
+
+
+            if (userInput < myNumber)
+            {
+                Console.WriteLine($"No. That number is too low!");
+            }
+            else if (userInput > myNumber)
+            {
+                Console.WriteLine($"No. Your number is too high!");
+            }
+            else
+            {
+                Console.WriteLine($"That's the correct guess. Good Job!");
+            }
         }
     }
 }
