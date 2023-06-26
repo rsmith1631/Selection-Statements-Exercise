7a51856fda485ec14cb1719da1ad93b3774e4822
rsmith1631 <rsmith1631@gmail.com>
2023-06-26 10:49:27 -0700

    Practice2 for my personal practice

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Guest the number that I'm thinking of!");
+
+            var myNumber = 25;
+
+            var userGuess = int.Parse(Console.ReadLine());
+
+            if(userGuess < myNumber) 
+            {
+                Console.Write($"Wrong! The number I'm thinking of is higher.";
+            }
+            else if(userGuess > myNumber) 
+            {
+                Console.WriteLine($"Incorrect! The number that I'm thinking of is lower.");
+            }
+            else 
+            {
+                Console.WriteLine($"Correct! You guessed 25. That's exactly the number I was thinking of");
+            }
         }
     }
 }
